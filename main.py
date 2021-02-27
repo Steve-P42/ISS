f44a3144c27e05d22edd0ee43435218a0fa6b8e1
Steve-P42 <78639312+Steve-P42@users.noreply.github.com>
2021-02-27 19:17:44 +0100

    creating basic program structure idea

---
+++ main.py
@@ -0,0 +1,68 @@
+# ----------------------------------------------------------------------------
+# Welcome to the real world, I hope you'll enjoy it!
+# Author:           Steve-P42
+# Description:      ISS - Information Request Interface
+# Creation date:    2021-02-27 16:07:45
+# Status:           in development
+# ----------------------------------------------------------------------------
+# %%
+
+def get_user_data():
+    """"get user data and save for further processing
+        use: https://randomuser.me/api/?inc=gender,name,location,nat
+    """
+    pass
+
+
+def display_message():
+    """define message template for requested data"""
+    pass
+
+
+def get_user_name():
+    pass
+
+
+def get_user_coordinates():
+    pass
+
+
+def get_user_city():
+    pass
+
+
+def get_iss_coordinates():
+    """fetch position via: http://api.open-notify.org/iss-now.json"""
+    pass
+
+
+def get_iss_pass_time():
+    """get info via: http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3"""
+    pass
+
+
+def get_number_of_crew():
+    """get info via: http://api.open-notify.org/astros.json"""
+    pass
+
+
+def calculate_distance():
+    pass
+
+
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
