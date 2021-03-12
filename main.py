7cf399e1212a24727da47874eba4686d4999fee8
Steve-P42 <78639312+Steve-P42@users.noreply.github.com>
2021-03-12 09:53:41 +0100

    resovled time issues

--- main.py
+++ main.py
@@ -9,12 +9,10 @@
 import urllib.request
 import json
 from datetime import datetime
-import tzlocal  # pip install tzlocal
 import haversine  # pip install haversine -> this is for distance calculations
 from geopy.geocoders import Nominatim  # pip install geopy -> to get coordinates by location
 
 
-# todo use asyncio to make the requests aat the same time
 class ISS:
     def __init__(self, name='Sir Prise', city='New York'):
         self.user_name = name  # self.get_user_name()
@@ -46,6 +44,8 @@ class ISS:
         Risetime: {self.three_passtimes[0][0]}  Duration: {self.three_passtimes[0][1]}
         Risetime: {self.three_passtimes[1][0]}  Duration: {self.three_passtimes[1][1]}
         Risetime: {self.three_passtimes[2][0]}  Duration: {self.three_passtimes[2][1]}
+        Risetime: {self.three_passtimes[3][0]}  Duration: {self.three_passtimes[3][1]}
+        Risetime: {self.three_passtimes[4][0]}  Duration: {self.three_passtimes[4][1]}
         
         Currently, there are {self.number_of_crew} astronauts on the ISS.
         Their names are: {self.crew_names}.
@@ -59,22 +59,14 @@ class ISS:
             location = geolocator.geocode(self.user_city)
 
             self.user_city = location.address
+            print(location.latitude, location.longitude)
             return location.latitude, location.longitude
-
-        # print(location.address)
-        # print((location.latitude, location.longitude))
-        # print(location.raw)
 
         except BaseException as e:
             print('Error message:', e)
             print('Location not found, used Paris as location instead.')
             self.user_city = 'Paris'
             return 48.8534, 2.3488  # Paris
-
-    # def get_user_city(self):
-    #     """User city name"""
-    #     location = input('What is your location?\n')
-    #     return location
 
     def get_iss_coordinates(self):
         """fetch position via: http://api.open-notify.org/iss-now.json"""
@@ -92,27 +84,25 @@ class ISS:
         risetime_and_duration1 = json_data['response'][0]
         risetime_and_duration2 = json_data['response'][1]
         risetime_and_duration3 = json_data['response'][2]
+        risetime_and_duration4 = json_data['response'][3]
+        risetime_and_duration5 = json_data['response'][4]
 
         r1 = self.convert_unix_time(risetime_and_duration1['risetime'])
         r2 = self.convert_unix_time(risetime_and_duration2['risetime'])
         r3 = self.convert_unix_time(risetime_and_duration3['risetime'])
+        r4 = self.convert_unix_time(risetime_and_duration4['risetime'])
+        r5 = self.convert_unix_time(risetime_and_duration5['risetime'])
         d1 = f"{risetime_and_duration1['duration'] // 60}:{risetime_and_duration1['duration'] % 60} (m:s)"
         d2 = f"{risetime_and_duration2['duration'] // 60}:{risetime_and_duration2['duration'] % 60} (m:s)"
         d3 = f"{risetime_and_duration3['duration'] // 60}:{risetime_and_duration3['duration'] % 60} (m:s)"
+        d4 = f"{risetime_and_duration4['duration'] // 60}:{risetime_and_duration4['duration'] % 60} (m:s)"
+        d5 = f"{risetime_and_duration5['duration'] // 60}:{risetime_and_duration5['duration'] % 60} (m:s)"
 
-        return (r1, d1), (r2, d2), (r3, d3)
-
-    # todo okay, something is off with the times^^, fix that
-    # compare to these:
-    # https://astroviewer.net/iss/de/beobachtung.php
-    # https://spotthestation.nasa.gov/sightings/view.cfm?country=Austria&region=None&city=Vienna
+        return (r1, d1), (r2, d2), (r3, d3), (r4, d4), (r5, d5)
 
     def convert_unix_time(self, unix_timestamp):
-        local_timezone = tzlocal.get_localzone()  # get pytz timezone
 
-        local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
-        # print(local_time.strftime("%d.%m.%Y %H:%M:%S %z (%Z)"))
-        return (local_time.strftime("%d.%m.%Y %H:%M:%S")# %z (%Z)"))
+        return datetime.fromtimestamp(unix_timestamp).strftime("%d.%m.%Y %H:%M:%S")
 
     def get_number_of_crew(self):
         """get info via: http://api.open-notify.org/astros.json"""
@@ -143,23 +133,7 @@ class ISS:
 # both in string format:
 
 
-new = ISS('Jason Bourne', 'London')
+new = ISS('Jason Bourne', 'Vienna')
 new.display_message()
 
-
 #%%
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="ISS_project")
-location = geolocator.geocode('vienna')
-print(location.raw)
-
-#todo use following resources to add timezone adaptation to the timestamps...
-#%% pip install timezonefinder   -> https://github.com/jannikmi/timezonefinder
-from timezonefinder import TimezoneFinder
-
-tf = TimezoneFinder()
-latitude, longitude = 48.8534, 2.3488
-tf.timezone_at(lng=longitude, lat=latitude)
-
-
-# ---> https://docs.python.org/3/library/zoneinfo.html
